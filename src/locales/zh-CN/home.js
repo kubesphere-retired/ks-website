82e5b627cb069ae25a4135f0ce23e6612b122846
Ray Zhou <ray@yunify.com>
2018-08-01 10:27:00 +0800

    re-formatting Chinese home page

--- src/locales/zh-CN/home.js
+++ src/locales/zh-CN/home.js
@@ -7,10 +7,10 @@ module.exports = {
   'Get Commercial Edition': '获取商业版',
   'What is KubeSphere': '什么是 KubeSphere ？',
   'KubeSphere is an open source project, built on Kubernetes, which is a powerful production-grade container orchestration system. KubeSphere provides users with easy-to-use interface and wizard design, it reduces the complexity of development, testing and maintenance, as well as lower the learning cost of the container orchestration platform.':
-    'KubeSphere 是一款开源项目，在目前主流容器调度平台 Kubernetes 之上构建的企业级分布式多租户容器管理平台，提供简单易用的操作界面以及向导式操作方式，在降低用户使用容器调度平台学习成本的同时，极大减轻开发、测试、运维的日常工作的复杂度',
+    'KubeSphere 是一款开源项目，在目前主流容器调度平台 Kubernetes 之上构建的企业级分布式多租户容器管理平台，提供简单易用的操作界面以及向导式操作方式，在降低用户使用容器调度平台学习成本的同时，极大减轻开发、测试、运维的日常工作的复杂度。',
   Easy: '易用',
   'KubeSphere providing friendly UI and wizard-design experience to users who work at development, testing or operation. Kubernetes’ powerful features could be delivered to users in an easiest way through the KubeSphere console.':
-    '面向开发、测试、运维友好的 UI ，向导式用户体验，通过 KubeSphere 控制台将 Kubernetes 的能力以一种极简的方式输送给用户。',
+    '面向开发、测试、运维友好的 UI，向导式用户体验，通过 KubeSphere 控制台将 Kubernetes 的能力以一种极简的方式输送给用户。',
   Flexible: '灵活',
   'Besides the open source solutions, if users would like to realize higher demands for network and storage, QingCloud can be used as the underlying platform to integrate QingCloud SDN, block storage and NeonSAN with service-guaranteed network and storage solutions.':
     '除开源解决方案外，如用户对网络和存储有更高要求，可选用青云作为底层平台，可以使用有服务保障的网络和存储解决方案，集成青云 SDN、块存储和 NeonSAN。',
@@ -19,17 +19,17 @@ module.exports = {
     '无基础设施依赖，无 Kubernetes 依赖，支持跨物理机、虚拟机、云平台部署，可以纳管不同版本、不同厂商的 Kubernetes 集群。',
   'Enable one-click deployment for application.': '应用一键部署',
   'Based on the open source OpenPitrix project, providing full lifecycle management of applications, including development, test, release, upgrade and remove, as well as multi-application registries management and one-click deployment of applications':
-    '基于开源的 OpenPitrix 项目，提供应用的全生命周期管理，包含开发，测试，发布，升级，下架等应用相关操作，多应用仓库管理，一键部署应用',
+    '基于开源的 OpenPitrix 项目，提供应用的全生命周期管理，包含开发、测试、发布、升级、下架等应用相关操作，还包括应用仓库管理、一键部署应用等。',
   'How to contribute': '如何参与贡献',
   'If you would like to participate in the development of KubeSphere upstream, or understand the progress of the project and report the bug, you can keep in touch with us through GitHub and Slack.':
-    '积极参与到 KubeSphere 上游的开发中，了解项目进度，以及 Bug 提交，可以通过 GitHub与 Slack 与我们保持联系',
-  'Community Alpha Edition': '社区版alpha',
-  'Community Beta Edition': '社区版beta',
-  'Community Edition 1.0': '社区版1.0',
+    '积极参与到 KubeSphere 上游的开发中，了解项目进度，以及 Bug 提交，可以通过 GitHub 与 Slack 与我们保持联系',
+  'Community Alpha Edition': '社区版 alpha',
+  'Community Beta Edition': '社区版 beta',
+  'Community Edition 1.0.0': '社区版 1.0.0',
   'Report the Bug': '报告Bug',
   'KubeSphere uses GitHub issue to manage bug tracking':
     'KubeSphere 使用 GitHub issue 来管理 bug 跟踪',
-  'Submit the Bug': '提交Bug',
+  'Submit the Bug': '提交 Bug',
   'Participate in development and design': '参与开发和设计',
   'Read the KubeSphere <a href="" target="_blank">Contributor Guide</a> carefully':
     '认真阅读 kubesphere <a href="" target="_blank">贡献者指南</a>',
