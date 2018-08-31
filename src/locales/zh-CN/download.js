9116bf4ecba2fb07583402d02808e80d01b82add
Ray Zhou <ray@yunify.com>
2018-08-31 14:38:49 +0800

    Update download Chinese page

--- src/locales/zh-CN/download.js
+++ src/locales/zh-CN/download.js
@@ -14,19 +14,19 @@ module.exports = {
   'Submit your contact information': '留下您的联系方式',
   'We have a professional pre-sales support team to provide you with pre-sales support': '我们有专业的售前团队为您提供支持',
   'We have received your trial application': '我们已收到了您的试用申请',
-  'More than 1000 users have already downloaded and used KubeSphere ,  please contact us via <a href="//github.com/kubesphere/kubesphere" target="_blank">Github</a> or <a href="//kubesphere.slack.com" target="_blank">Slack</a> if you have any questions.': '已经有超过 1000 名用户下载并使用了KubeSphere，如有问题可通过 <a href="//github.com/kubesphere/kubesphere" target="_blank">Github</a> 或者 <a href="//kubesphere.slack.com" target="_blank">Slack</a> 与我们联系。',
+  'More than 1000 users have already downloaded and used KubeSphere ,  please contact us via <a href="//github.com/kubesphere/kubesphere" target="_blank">Github</a> or <a href="//kubesphere.slack.com" target="_blank">Slack</a> if you have any questions.': '已经有超过 1000 名用户下载并使用了 KubeSphere，如有问题可通过 <a href="//github.com/kubesphere/kubesphere" target="_blank">Github</a> 或者 <a href="//kubesphere.slack.com" target="_blank">Slack</a> 与我们联系。',
   'To get the latest KubeSphere news via leaving your contact information': '留下您的联系方式，获取最新的 KubeSphere 的动态信息。',
   'Do not want to be frequently notified by email': '不希望经常收到邮件通知',
   'Thank you for downloading KubeSphere': '感谢您下载 KubeSphere',
   'If you have any questions, see the <a href="//docs.kubesphere.io/express/zh-CN/KubeSphere-Installer-Guide/" target="_blank">KubeSphere Installation Guide</a>': '如果您遇到问题，可参阅<a href="//docs.kubesphere.io/express/zh-CN/KubeSphere-Installer-Guide/" target="_blank">部署文档</a>。',
   'More Options': '更多选择',
   'If you need to install offline please select the <a href="//kubesphere.anybox.qingcloud.com/s/UBBctImJLtxnkqT5hrEaijjolTENNVsr" target="_blank">offline installation package</a>': '如果您需要离线安装请选择<a href="//kubesphere.anybox.qingcloud.com/s/UBBctImJLtxnkqT5hrEaijjolTENNVsr" target="_blank">离线安装包</a>',
-  'If you already have a Kubernetes cluster, you can select <a href="" target="_blank"> KS-Core installation </a>': '如果您已有 Kubernetes 集群您可以选择 <a href="" target="_blank">Ks-Core 安装</a>',
+  'If you already have a Kubernetes cluster, you can select <a href="" target="_blank"> KS-Core installation </a>': '如果您已有 Kubernetes 集群您可以选择 <a href="" target="_blank">KS-Core 安装</a>',
   'Please keep your email and phone works so that our pre-sales team can communicate with you as soon as possible': '请您保持联系方式的畅通，以便于我们售前团队与您及时沟通',
   'You can also download the trial version': '您也可以下载试用版',
   'Online Installation': '在线安装',
   'Offline Installation': '离线安装',
-  'Welcome to the KubeSphere Community Edition, it\'s only recommended to understand the KubeSphere features. For production environment we recommend you to purchase the Commercial Edition.': '欢迎使用Kubesphere社区版，仅建议您用来了解 KubeSphere 功能特性,生产环境建议您购买商业版',
+  'Welcome to the KubeSphere Community Edition, it\'s only recommended to understand the KubeSphere features. For production environment we recommend you to purchase the Commercial Edition.': '欢迎使用 KubeSphere 社区版，仅建议您用来了解 KubeSphere 功能特性,生产环境建议您购买商业版',
   'KubeSphere provides workload and cluster management, wizard design operation interface, application repository and application template management, as well as our professional team support.': '提供工作负载及集群管理，可视化图形操作界面，应用及应用模板管理，并提供专业的团队服务支持',
-  'KubeSphere provides workload management, CI/CD and Microservices governance, multi-cluster management, as well as multi-tenancy and fine-grained privilege separation for users of different enterprise scales.': '提供工作负载，CI/CD以及微服务治理平台，多集群管理，多租户以及细粒度的权限划分，适用于不同企业规模的用户'
+  'KubeSphere provides workload management, CI/CD and Microservices governance, multi-cluster management, as well as multi-tenancy and fine-grained privilege separation for users of different enterprise scales.': '提供工作负载，CI/CD 以及微服务治理平台，多集群管理，多租户以及细粒度的权限划分，适用于不同企业规模的用户'
 }
