5f1430e15d1d9ff871783fe88e6daae97cdbde8f
liulu <liulu897292197@qq.com>
2019-09-05 11:15:21 +0800

    update: Add wechat codes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,131 +3,188 @@ import React from 'react'
 import classnames from 'classnames'
 import { withTranslation } from 'react-i18next'
 
+import tippy from 'tippy.js'
+
 import Logo from '../Logo'
 import Language from '../Language/index'
 
+import { ReactComponent as WechatIcon } from '../../assets/wechat.svg'
+import { ReactComponent as GroupIcon } from '../../assets/group.svg'
+import { ReactComponent as VideoIcon } from '../../assets/video.svg'
+import { ReactComponent as SlackIcon } from '../../assets/slack.svg'
+import WechatCode from '../../assets/wechat_code.svg'
+import GroupCode from '../../assets/group_code.svg'
+
 import styles from './index.module.scss'
 
-const Footer = ({ className, t, pageContext }) => (
-  <div className={classnames(styles.footer, className)}>
-    <div className={styles.wrapper}>
-      <div className={styles.info}>
-        <div style={{ display: 'inline-block' }}>
-          <Logo className={styles.logo} />
-          <Language pageContext={pageContext} />
+class Footer extends React.Component {
+  componentDidMount() {
+    tippy('#wechat', {
+      content: document.getElementById('wechat_tip').innerHTML,
+      arrow: true,
+      arrowType: 'round',
+      theme: 'light',
+      interactive: true,
+    })
+    tippy('#group', {
+      content: document.getElementById('group_tip').innerHTML,
+      arrow: true,
+      arrowType: 'round',
+      theme: 'light',
+      interactive: true,
+    })
+  }
+
+  render() {
+    const { className, t, pageContext } = this.props
+    return (
+      <div className={classnames(styles.footer, className)}>
+        <div className={styles.wrapper}>
+          <div className={styles.info}>
+            <div style={{ display: 'inline-block' }}>
+              <Logo className={styles.logo} />
+              <Language pageContext={pageContext} />
+            </div>
+            <div className={styles.links}>
+              <ul>
+                <li>
+                  <div className="h3">{t('Products')}</div>
+                  <a href={`/${pageContext.locale}/`}>{t('Express Edition')}</a>
+                  <a
+                    href="https://appcenter.qingcloud.com/apps/app-cmgbd5k2/KubeSphere%C2%AE%EF%B8%8F%EF%BC%88QKE%EF%BC%89#Introduction"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('QKE')}
+                  </a>
+                  <a
+                    href="https://kubesphere.qingcloud.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('Commercial Editions')}
+                  </a>
+                </li>
+                <li>
+                  <div className="h3">{t('Related Products')}</div>
+                  <a
+                    href="https://appcenter.qingcloud.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    AppCenter
+                  </a>
+                  <a
+                    href="https://www.qingcloud.com/products/qingstor-neonsan/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    NeonSAN
+                  </a>
+                  <a
+                    href="https://www.qingcloud.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    QingCloud
+                  </a>
+                  <a
+                    href="https://openpitrix.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    OpenPitrix
+                  </a>
+                </li>
+                <li>
+                  <div className="h3">{t('KubeSphere Docs')}</div>
+                  <a
+                    href="/docs/advanced-v2.0/zh-CN/introduction/intro/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('Introduction')}
+                  </a>
+                  <a
+                    href="/docs/advanced-v2.0/zh-CN/installation/intro/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('Installation')}
+                  </a>
+                  <a
+                    href="/docs/advanced-v2.0/zh-CN/quick-start/quick-start-guide/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('Tutorial')}
+                  </a>
+                  <a
+                    href="/docs/advanced-v2.0/zh-CN/api-reference/api-docs/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('API Documentation')}
+                  </a>
+                </li>
+                <li>
+                  <div className="h3">{t('About')}</div>
+                  <a
+                    href="//activity.qingcloud.com/event/kubesphere2019/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('News')}
+                  </a>
+                  <a
+                    href="//www.qingcloud.com/contactus"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('Contact')}
+                  </a>
+                  <a
+                    href="//www.qingcloud.com/jobs"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('Careers')}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.logos}>
+            <WechatIcon id="wechat" />
+            <GroupIcon id="group" />
+            <a href="https://www.youtube.com/channel/UCybIhsLg8P1xzCx97eELiwQ" target="_blank">
+              <VideoIcon />
+            </a>
+            <a
+              href="https://join.slack.com/t/kubesphere/shared_invite/enQtNTE3MDIxNzUxNzQ0LTZkNTdkYWNiYTVkMTM5ZThhODY1MjAyZmVlYWEwZmQ3ODQ1NmM1MGVkNWEzZTRhNzk0MzM5MmY4NDc3ZWVhMjE"
+              target="_blank"
+            >
+              <SlackIcon />
+            </a>
+          </div>
+          <p className={styles.icp}>KubeSphere®️ 2019 All Rights Reserved.</p>
         </div>
-        <div className={styles.links}>
-          <ul>
-            <li>
-              <div className="h3">{t('Products')}</div>
-              <a href={`/${pageContext.locale}/`}>{t('Express Edition')}</a>
-              <a
-                href="https://appcenter.qingcloud.com/apps/app-cmgbd5k2/KubeSphere%C2%AE%EF%B8%8F%EF%BC%88QKE%EF%BC%89#Introduction"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('QKE')}
-              </a>
-              <a
-                href="https://kubesphere.qingcloud.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('Commercial Editions')}
-              </a>
-            </li>
-            <li>
-              <div className="h3">{t('Related Products')}</div>
-              <a
-                href="https://appcenter.qingcloud.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                AppCenter
-              </a>
-              <a
-                href="https://www.qingcloud.com/products/qingstor-neonsan/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                NeonSAN
-              </a>
-              <a
-                href="https://www.qingcloud.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                QingCloud
-              </a>
-              <a
-                href="https://openpitrix.io"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                OpenPitrix
-              </a>
-            </li>
-            <li>
-              <div className="h3">{t('KubeSphere Docs')}</div>
-              <a
-                href="/docs/advanced-v2.0/zh-CN/introduction/intro/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('Introduction')}
-              </a>
-              <a
-                href="/docs/advanced-v2.0/zh-CN/installation/intro/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('Installation')}
-              </a>
-              <a
-                href="/docs/advanced-v2.0/zh-CN/quick-start/quick-start-guide/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('Tutorial')}
-              </a>
-              <a
-                href="/docs/advanced-v2.0/zh-CN/api-reference/api-docs/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('API Documentation')}
-              </a>
-            </li>
-            <li>
-              <div className="h3">{t('About')}</div>
-              <a
-                href="//activity.qingcloud.com/event/kubesphere2019/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('News')}
-              </a>
-              <a
-                href="//www.qingcloud.com/contactus"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('Contact')}
-              </a>
-              <a
-                href="//www.qingcloud.com/jobs"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t('Careers')}
-              </a>
-            </li>
-          </ul>
+        <div id="wechat_tip" style={{ display: 'none' }}>
+          <div className={styles.tooltip}>
+            <div>{t('Follow the official account')}</div>
+            <img src={WechatCode} alt="" />
+          </div>
+        </div>
+        <div id="group_tip" style={{ display: 'none' }}>
+          <div className={styles.tooltip}>
+            <div>{t('Join the WeChat group')}</div>
+            <img src={GroupCode} alt="" />
+            <small>进群请备注 “公司 - 姓名”</small>
+          </div>
         </div>
       </div>
-      <p className={styles.icp}>KubeSphere®️ 2019 All Rights Reserved.</p>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default withTranslation()(Footer)
