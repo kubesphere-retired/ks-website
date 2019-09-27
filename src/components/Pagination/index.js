174a65634d2db6a2f01a9458f79285c1a4d9cc58
liulu <liulu897292197@qq.com>
2019-09-27 18:21:16 +0800

    fix: Fix typo

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 
-import { ReactComponent as ArrowIcon } from '../../assets/Paging.svg'
+import { ReactComponent as ArrowIcon } from '../../assets/paging.svg'
 
 import styles from './index.module.scss'
 
